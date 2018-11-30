f8a26178aec503e41a6c490d9bf80504b1566a51
Elizabeth Villalejos <nekobanpaia@gmail.com>
2018-11-30 15:24:51 -0700

    Fixed code

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
 
-}
+var removeFromArray = function(...args) {
+    const array = args[0]
+    return array.filter(val => !args.includes(val))
+  }
 
 module.exports = removeFromArray
