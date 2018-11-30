8e002317efadf60ad60fdbd12f2431e4d76be021
Elizabeth Villalejos <nekobanpaia@gmail.com>
2018-11-29 17:35:09 -0700

    Functions exercise repeat a string X given times

---
+++ repeatString/repeatString.js
@@ -0,0 +1,12 @@
+var repeatString = function(string, number) {
+    if (number < 0) return 'ERROR';
+    let str = [];
+    while (0 < number) {
+      str.push(string);
+      number -= 1;
+    }
+  
+    return str.join('');
+  }
+  
+  module.exports = repeatString
