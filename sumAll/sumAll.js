abd390d2dafa67c5b24494dc5b89d1c9022ed8bd
Elizabeth Villalejos <nekobanpaia@gmail.com>
2018-12-03 12:59:31 -0700

    Completed exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0){ 
+        return 'ERROR';
+    }
+    if (typeof min !== "number" || typeof max !== "number"){
+        return 'ERROR';
+    }
 
+    if (min > max){
+        const x = min;
+        min = max;
+        max = x;
+    }
+    
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 module.exports = sumAll
